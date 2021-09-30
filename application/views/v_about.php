6277d4dbe12f3cce139ab506509ea97d598c8910
Chelseanillesia <chelseanillesia@gmail.com>
2021-09-30 21:41:25 +0700

    Commit tugas 3

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Chelsea</li>
+        <li>Nama Belakang : Nillesia</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jl. Raya Lintas Malindo kec Beduai</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Beduai</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+       <li>Jalan kaki</li>
+       <li>Bernafas</li>
+    </ul>
+</section>
